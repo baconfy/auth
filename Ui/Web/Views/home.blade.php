6095750fbe75834c5f24098face7b8968bb5fa68
Renato Dehnhardt <renatotkd@gmail.com>
2020-02-23 17:33:34 +0100

    add home template

--- Ui/Web/Views/home.blade.php
+++ Ui/Web/Views/home.blade.php
@@ -1,19 +1,7 @@
-@extends('ui::layouts.auth')
+@extends('ui::layouts.app')
 
-@section('title', __('auth::verify.title'))
+@section('title', __('dashboard::home.title'))
 
 @section('content')
-    @if (session('resent'))
-        <div class="alert alert-success" role="alert">
-            {{ __('auth::verify.alert') }}
-        </div>
-    @endif
-
-    <p>{{ __('auth::verify.welcome') }}</p>
-    {{ __('auth::verify.before') }}
-    {{ __('auth::verify.receive') }},
-    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-        @csrf
-        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('auth::verify.request') }}</button>.
-    </form>
+    <h1>{{ __('dashboard::home.title') }}</h1>
 @endsection
